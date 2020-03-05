e9e61168c78c881423084db185dcbaee9b048509
ScorpionOrange <scorpionorange@126.com>
2020-03-05 23:37:57 +0800

    Initial commit

---
+++ app/src/main/java/com/example/compassinkotlin/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.compassinkotlin
+
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity(), SensorEventListener{
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
